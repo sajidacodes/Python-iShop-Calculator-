cf3abab7469c0d0d160f691e206d013fef197289
sajidacodes <soupark7@gmail.com>
2025-10-04 23:53:04 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,33 @@
+﻿# iShop Calculator Project
+
+## Project Description
+This project is a simple Python program that helps users calculate the total cost of items in their shopping basket. 
+It asks for the number of items, the name of each item, and its price, then shows the full basket and total cost.
+
+## How to Use
+1. Run the file ishopping_calculator.py.
+2. Enter the number of items in your basket.
+3. For each item, enter its name and price.
+4. You can choose to see your basket items.
+5. You can choose to see the total cost.
+
+## Example
+If you enter 3 items:
+- Item 1: Apple $2.5
+- Item 2: Bread $1.2
+- Item 3: Milk $1.5
+
+The program will show:
+--------
+- Apple
+- Bread
+- Milk
+--------
+💰 Buying these items will cost: $5.20
+--------
+
+## Features
+- User-friendly and interactive.
+- Handles any number of items.
+- Shows the basket and total cost clearly.
+- Prices are displayed with two decimal points
---
+++ ishopping_calculator.py
@@ -0,0 +1,39 @@
+﻿basket_items = []
+much_basket = []
+
+print("\n***** Welcome to iShop calculator *****\n")
+basket1 = int(input("how many items are there in your basket today?\n"))
+if basket1 >0:
+ print("let's get to counting them....")
+ for basket in range(1,basket1+1):
+
+   name_basket = input(f"Please tell me the name of the item number {basket}")
+   basket_items.append(name_basket)
+   price = float(input(f"What is the price of {name_basket} \n$"))
+   much_basket.append(price)
+ basket_items2 =input("Would you like to see your entire basket items?(Yes/No)\n").lower()
+ if basket_items2 == "yes":
+   print("--------")
+   for ii in basket_items:
+     print(f"-{ii}")
+     print("--------")
+ else:
+  print("Press Enter to exit...")
+ much = input("Would you like to see how much it'll cost? (Yes/No)\n").lower()
+ if much == "yes":
+  print("--------")
+  print(f"Buying these items will cost:")
+  print(f"${sum(much_basket):.2f}")
+  print("--------")
+ else:
+  print("Press Enter to exit...")
+else:
+  print("It seems you don't want to buy.")
+
+  
+    
+
+    
+
+ 
+
